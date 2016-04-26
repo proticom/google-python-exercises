d2bcede7987de77489ee2ed9ff4880570146fb4a
proticom <etadros@proticom.com>
2016-04-26 12:07:00 -0700

    Copy Special Exercises

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -17,8 +17,39 @@ import commands
 
 # +++your code here+++
 # Write functions and modify main() to call them
+## Example pulls filenames from a dir, prints their relative and absolute paths
+def validate_dir(dir):
+  if os.path.exists(dir)==False:
+    os.makedirs(dir)
+  return
 
 
+def get_special_paths(dir):
+  l_files = []
+  filenames = os.listdir(dir)
+  for filename in filenames:
+    match = re.search(r'__\w+__',filename)
+    if match:
+      f_path = os.path.join(dir,filename)
+      f_abspath = os.path.abspath(f_path)
+      l_files.append(f_abspath)
+  return l_files
+
+def copy_to(from_dir,to_dir):
+  validate_dir(to_dir)
+  for path in get_special_paths(from_dir):
+    shutil.copy(path,to_dir)
+
+
+def zip_to(paths,zippath):
+  l_files = get_special_paths(paths)
+  cmd = 'zip -j ' + zippath + ' ' + ' '.join(l_files)
+  print "Command to run:", cmd   ## good to debug cmd before actually running it
+  (status, output) = commands.getstatusoutput(cmd)
+  if status:    ## Error case, print the command's output to stderr and exit
+    sys.stderr.write(output)
+    sys.exit(1)
+  print output  ## Otherwise do something with the command's output
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -50,6 +81,16 @@ def main():
 
   # +++your code here+++
   # Call your functions
+  if todir == '' and tozip == '':
+    for file in get_special_paths(args[0]):
+      print file
+
+  if todir != '':
+    copy_to(args[0],todir)
+
+  if tozip != '':
+    zip_to(args[0],tozip)
+
   
 if __name__ == "__main__":
   main()
